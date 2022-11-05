a33e37df0591985319c801ff27c1b552977186b1
Ben Coleman <benc.uk@gmail.com>
2022-11-05 02:00:06 +0000

    🐍 snek part1

--- Makefile
+++ Makefile
@@ -60,5 +60,5 @@ watch: ## 👀 Run the game with reload on file change
 
 publish: build ## 🎁 Bundle distribution files
 	@$(BIN)/w4 bundle $(BUILD)/cart.wasm --html $(OUT)/index.html --title "Snake Game"
-#@$(BIN)/w4 bundle $(BUILD)/cart.wasm --linux $(OUT)/game
-#@$(BIN)/w4 bundle $(BUILD)/cart.wasm --windows $(OUT)/game.exe
+	@$(BIN)/w4 bundle $(BUILD)/cart.wasm --linux $(OUT)/game
+	@$(BIN)/w4 bundle $(BUILD)/cart.wasm --windows $(OUT)/game.exe
--- main.go
+++ main.go
@@ -2,27 +2,65 @@ package main
 
 import "cart/w4" //nolint:all
 
-var smiley = [8]byte{
-	0b11000011,
-	0b10000001,
-	0b00100100,
-	0b00100100,
-	0b00000000,
-	0b00100100,
-	0b10011001,
-	0b11000011,
+var (
+	snake = &Snake{
+		Body: []Point{
+			{X: 2, Y: 0},
+			{X: 1, Y: 0},
+			{X: 0, Y: 0},
+		},
+		Direction: Point{X: 1, Y: 0},
+	}
+	frameCount = 0
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+type Snake struct {
+	Body      []Point
+	Direction Point
+}
+
+func (s *Snake) Draw() {
+	*w4.DRAW_COLORS = 0x0042
+	for _, part := range s.Body {
+		w4.Rect(part.X*8, part.Y*8, 8, 8)
+	}
+
+	*w4.DRAW_COLORS = 0x0004
+	w4.Rect(s.Body[0].X*8, s.Body[0].Y*8, 8, 8)
+}
+
+func (s *Snake) Update() {
+	for i := len(s.Body) - 1; i > 0; i-- {
+		s.Body[i] = s.Body[i-1]
+	}
+	s.Body[0].X = (s.Body[0].X + s.Direction.X) % 20
+	s.Body[0].Y = (s.Body[0].Y + s.Direction.Y) % 20
+	if s.Body[0].X < 0 {
+		s.Body[0].X = 19
+	}
+	if s.Body[0].Y < 0 {
+		s.Body[0].Y = 19
+	}
+}
+
+//go:export start
+func start() {
+	w4.PALETTE[0] = 0x2d1b00
+	w4.PALETTE[1] = 0x1e606e
+	w4.PALETTE[2] = 0x5ab9a8
+	w4.PALETTE[3] = 0xc4f0c2
 }
 
 //go:export update
 func update() {
-	*w4.DRAW_COLORS = 2
-	w4.Text("Hello from Go!", 10, 10)
-
-	var gamepad = *w4.GAMEPAD1
-	if gamepad&w4.BUTTON_1 != 0 {
-		*w4.DRAW_COLORS = 4
+	frameCount++
+	if frameCount%9 == 0 {
+		snake.Update()
 	}
-
-	w4.Blit(&smiley[0], 76, 76, 8, 8, w4.BLIT_1BPP)
-	w4.Text("Press X if you like!", 16, 90)
+	snake.Draw()
 }
